0cd1aa070b4b58bfdb70b21eca660bace7eb1475
nonoka57 <nonoka57nonoka@gmail.com>
2021-10-14 07:25:49 +0000

    [add]home

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,20 @@
+class BooksController < ApplicationController
+  
+  def index
+  end
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end  
+  
+  
+  
+end
