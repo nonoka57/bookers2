50330576eed97df802bdb727a6198b442d5b604c
nonoka57 <nonoka57nonoka@gmail.com>
2021-10-17 08:14:17 +0000

    [add]book_edit,destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,22 +7,40 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.user.id = current_user.id
+    @book.user_id = current_user.id
     @book.save
     redirect_to books_path
   end
 
   def show
+    @books = Book.find(params[:id])
+    @book = Book.new
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @books = Book.find(params[:id])
+    @books.destroy
+    redirect_to books_path
   end
 
-  
-  private
-  
-  def book_params
-    params.require(:book).premit(:title, :body)
 
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   
+  def show
+  @user = User.find(paramu[:id])
+  end
   
 end
