9eb962d9a1683775121a40b87179432dd8d00cd9
nonoka57 <nonoka57nonoka@gmail.com>
2021-11-01 14:08:30 +0000

    [fix]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,16 +1,17 @@
 class BookCommentsController < ApplicationController
+  
 
   def create
     book= Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id=book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    redirect_to book_path(book.id)
   end
 
   def destroy
     BookComment.find_by(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    redirect_to book_path(params[:book_id])
   end
 
   private
@@ -18,6 +19,10 @@ class BookCommentsController < ApplicationController
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
+  
+
+
+ 
 
 
 end
