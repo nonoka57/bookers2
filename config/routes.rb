74145aaf2e67d33164ba11da9d642e0281f622b0
nonoka57 <nonoka57nonoka@gmail.com>
2021-11-02 11:16:36 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   root to: "homes#top"
   get "/home/about" => "homes#about"
-  devise_for :users
+  devise_for :users 
+  
+  resources :users, only: [:index, :show, :edit, :update] do
+      resource :relationships, only: [:create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+  end
+      
+      
   resources :books, only: [ :create, :index, :show, :edit, :update, :destroy] do
    resource :favorites, only: [:create, :destroy]
    resource :book_comments, only: [:create, :destroy]
