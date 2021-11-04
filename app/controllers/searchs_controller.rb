600bc2dcff98a0be09a3a2af9990ca1fc5188949
nonoka57 <nonoka57nonoka@gmail.com>
2021-11-04 09:18:14 +0000

    [add]前方、後方

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,28 +1,42 @@
 class SearchsController < ApplicationController
-  
+
   def search
     @model = params["model"]
     @content = params["content"]
     @method = params["method"]
     @records = search_for(@model, @content, @method)
   end
-  
+
   private
   def search_for(model, content, method)
+
     if model == 'user'
-      if method == 'perfect'
+      if method == "forword"
+        User.where("name LIKE?","#{content}%")
+      elsif method == "backword"
+        User.where("name LIKE?","%#{content}")
+      elsif method == "perfect"
         User.where(name: content)
+      elsif method == "partial"
+        User.where("name LIKE?","%#{content}%")
       else
-        User.where('name LIKE ?', '%'+content+'%')
+        User.all
       end
+      
     elsif model == 'book'
-      if method == 'perfect'
+      if method == "forword"
+        Book.where("title LIKE?", "#{content}%")
+      elsif method == "backword"
+        Book.where("title LIKE?", "%#{content}")
+      elsif method == "perfect"
         Book.where(title: content)
+      elsif method == "partial"
+        Book.where("title LIKE?", "%#{content}%")
       else
-        Book.where('title LIKE ?', '%'+content+'%')
+        Book.all
       end
     end
   end
-  
+
 end
 
